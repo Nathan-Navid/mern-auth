685126f74f73ca0f8b0401572367c07ca74023d2
NathanNavid <naaghajafari@gmail.com>
2023-05-31 17:12:06 -0700

    creating log in and log out,

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -29,6 +29,35 @@ module.exports = {
         } catch (err) {
             return res.json(err);
         }
+    },
+    // login an existing user
+    login: async (req, res) => {
+        try {
+            const user = await UserModel.findOne({ email: req.body.email });
+            if (user) {
+                const passwordMatch = await bcrypt.compare(req.body.password, user.password); // if const unicorn it should be unicorn.password
+                // it will be a boolean response
+                if (passwordMatch) {
+                    const userToken = jwt.sign({ _id: user._id, email: user.email }, secret, { expiresIn: "2h" });
+                    res.cookie("usertoken", userToken, secret, { httpOnly: true }).json({
+                        message: "Success!",
+                        user: user
+                    });
+                }
+                else {
+                    res.status(400).json({
+                        message: "invalid login attempt"
+                    });
+                }
+
+            } else {
+                res.status(400).json({
+                    message: "Invalid login attempt"
+                });
+            }
+        } catch (err) {
+            return res.json(err);
+        }
     }
 
 }
